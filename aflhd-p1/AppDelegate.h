7b9e43775f0c453f41b6ff8b5e50fc141f1a018f
tkumata <tkumata@gmail.com>
2015-11-10 21:21:25 +0900

    Initial Commit

---
+++ aflhd-p1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aflhd-p1
+//
+//  Created by KUMATA Tomokatsu on 11/10/15.
+//  Copyright © 2015 KUMATA Tomokatsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ aflhd-p1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aflhd-p1
+//
+//  Created by KUMATA Tomokatsu on 11/10/15.
+//  Copyright © 2015 KUMATA Tomokatsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
